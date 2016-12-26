1e17b9e02f30eb63bbcd048d0b9d9b20b0dc431e
John Hawthorn <john.hawthorn@gmail.com>
2016-12-25 21:08:19 -0800

    Add finalizer to session

--- test.rb
+++ test.rb
@@ -14,7 +14,20 @@ class TmuxDriver
   class TmuxError < StandardError
   end
 
-  class Session < Struct.new(:driver, :name)
+  class Session
+    attr_reader :driver, :name
+
+    def initialize(driver, name)
+      @driver = driver
+      @name = name
+
+      ObjectSpace.define_finalizer(self, self.class.finalize(driver, name))
+    end
+
+    def self.finalize(driver, name)
+      proc { driver.tmux(*%W[kill-session -t #{name}]) }
+    end
+
     def capture
       driver.tmux(*%W[capture-pane -t #{name} -p])
     end
