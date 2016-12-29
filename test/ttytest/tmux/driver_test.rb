1c8074ee4fec363f93b53359ef7370d77ebb193d
John Hawthorn <john.hawthorn@gmail.com>
2016-12-29 02:17:42 -0800

    Test failure case with too old tmux

---
+++ test/ttytest/tmux/driver_test.rb
@@ -0,0 +1,52 @@
+
+require "test_helper"
+
+module TTYtest
+  class TmuxDriverTest < Minitest::Test
+    def test_availability
+      # Using actual tmux
+      @driver = TTYtest::Tmux::Driver.new
+      assert @driver.available?
+
+      # Using fake tmux which just prints 1.8
+      with_fake_tmux_command('1.8') do |cmd|
+        @driver = TTYtest::Tmux::Driver.new(command: cmd)
+        assert @driver.available?
+      end
+
+      with_fake_tmux_command('2.0') do |cmd|
+        @driver = TTYtest::Tmux::Driver.new(command: cmd)
+        assert @driver.available?
+      end
+
+      with_fake_tmux_command('1.7') do |cmd|
+        @driver = TTYtest::Tmux::Driver.new(command: cmd)
+        assert !@driver.available?
+        assert_raises TTYtest::Tmux::Driver::TmuxError, "tmux version 1.7 does not meet requirement >= 1.8" do
+          @driver.new_terminal('')
+        end
+      end
+
+      @driver = TTYtest::Tmux::Driver.new(command: "tmux_command_not_found")
+      assert !@driver.available?
+      assert_raises TTYtest::Tmux::Driver::TmuxError, "tmux version 1.7 does not meet requirement >= 1.8" do
+        @driver.new_terminal('')
+      end
+    end
+
+    def with_fake_tmux_command(version)
+      file = Tempfile.new('ttytest_fake_tmux')
+      begin
+        file.chmod(0700)
+        file.puts <<RUBY
+echo 'tmux #{version}'
+RUBY
+        file.close
+        yield file.path
+      ensure
+        file.close
+        file.unlink   # deletes the temp file
+      end
+    end
+  end
+end
