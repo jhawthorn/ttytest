b77b9a29322dbad7e7493dcc561789c0c9dc00a3
John Hawthorn <john.hawthorn@gmail.com>
2017-01-02 00:24:52 -0800

    Add tests for TTYtest itself

--- test/ttytest_test.rb
+++ test/ttytest_test.rb
@@ -1,4 +1,15 @@
 require "test_helper"
 
 class TTYtestTest < Minitest::Test
+  def test_default_driver
+    assert_instance_of TTYtest::Tmux::Driver, TTYtest.driver
+  end
+
+  def test_default_max_wait_time
+    assert_equal 2, TTYtest.default_max_wait_time
+  end
+
+  def test_new_terminal
+    assert_instance_of TTYtest::Terminal, TTYtest.new_terminal('')
+  end
 end
